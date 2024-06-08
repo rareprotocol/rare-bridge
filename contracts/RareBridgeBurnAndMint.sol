b83a3fbd63f7f359570201058a12b4a70dc71665
koteld <festbot@gmail.com>
2024-06-08 17:14:35 +0200

    update dependencies: add IBurnMintERC20.sol, remove custom implementation

--- contracts/RareBridgeBurnAndMint.sol
+++ contracts/RareBridgeBurnAndMint.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
+import {IBurnMintERC20} from "@chainlink/contracts-ccip/src/v0.8/shared/token/ERC20/IBurnMintERC20.sol";
 import {RareBridge} from "./RareBridge.sol";
-import "./IERC20MintableBurnable.sol";
 
 /// @title Rare Bridge: Burn and Mint
 /// @notice The bridge that sends and receives RARE tokens and arbitrary messages.
@@ -16,7 +16,7 @@ contract RareBridgeBurnAndMint is RareBridge {
   /// @param _amount The amount of RARE tokens to burn.
   function _handleTokensOnSend(address _sender, uint256 _amount) internal override returns (bool success) {
     // Burn tokens from sender, reducing the total supply.
-    return IERC20MintableBurnable(s_rareToken).burnFrom(_sender, _amount);
+    IBurnMintERC20(s_rareToken).burnFrom(_sender, _amount);
   }
 
   /// @notice Mint RARE tokens to a recipient.
@@ -24,6 +24,6 @@ contract RareBridgeBurnAndMint is RareBridge {
   /// @param _amount The amount of RARE tokens to mint.
   function _handleTokensOnReceive(address _to, uint256 _amount) internal override returns (bool success) {
     // Mint tokens for a recipient, increasing the total supply.
-    return IERC20MintableBurnable(s_rareToken).mint(_to, _amount);
+    IBurnMintERC20(s_rareToken).mint(_to, _amount);
   }
 }
